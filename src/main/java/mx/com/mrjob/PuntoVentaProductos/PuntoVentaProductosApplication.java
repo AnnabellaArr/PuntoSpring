5c2bc1c6cf333df69f6ac1171312d097e3e2652e
AnnabellArr <ana2527star@gmail.com>
2026-03-06 14:34:00 -0600

    Primer commit

---
+++ src/main/java/mx/com/mrjob/PuntoVentaProductos/PuntoVentaProductosApplication.java
@@ -0,0 +1,13 @@
+package mx.com.mrjob.PuntoVentaProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoVentaProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoVentaProductosApplication.class, args);
+	}
+
+}
